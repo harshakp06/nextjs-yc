0d9460badd98d31472ef5c6f1c8b29db1f3dc84c
harshakp06 <kpharsha@yahoo.com>
2024-12-26 17:38:51 +0530

    added sanity

--- yc/components/StartupCard.tsx
+++ yc/components/StartupCard.tsx
@@ -1,16 +1,18 @@
 import { formatDate } from "@/lib/utils";
 import React from "react";
-import StartupCardType from "./StartupCardType";
 import { EyeIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Author, Startup } from "@/sanity/types";
+
+export type StartupCardType = Omit<Startup, "author"> & {author?: Author}
 
 const StartupCard = ({ post }: { post: StartupCardType }) => {
   const {
     _createdAt,
     views,
-    author: { id: authorId, name },
+    author,
     title,
     category,
     _id,
@@ -30,14 +32,14 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
 
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${authorId}`}>
-            <p className="text-16-medium line-clamp-1">{name}</p>
+          <Link href={`/user/${author?._id}`}>
+            <p className="text-16-medium line-clamp-1">{author?.name}</p>
           </Link>
           <Link href={`/startup/${_id}`}>
             <h3 className="text-26-semibold">{title}</h3>
           </Link>
         </div>
-        <Link href={`/user/${authorId}`}>
+        <Link href={`/user/${author?._id}`}>
           <Image
             src="https://placehold.co/48x48"
             alt="placeholder"
@@ -58,7 +60,7 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
       </Link>
 
       <div className="flex-between gap-3 mt-5">
-        <Link href={`/query=${category.toLowerCase()}`}>
+        <Link href={`/query=${category?.toLowerCase()}`}>
           <span className="text-16-medium text-primary">{category}</span>
         </Link>
 
